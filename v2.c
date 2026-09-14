7f29ef3dcf79a386b29f858e800272ff3f124201
Thareekh <64839252+thareekh@users.noreply.github.com>
2026-09-14 22:20:28 +0530

    Update v2.c

--- v2.c
+++ v2.c
@@ -12,7 +12,7 @@ int main(int argc,char **argv)
 
         if(argc!=3)
         {
-                printf("USAGE: ./a.out encrypt/decrypt fileName\n");
+                printf("USAGE: ./xsecure encrypt/decrypt fileName\n");
                 return 1;
         }
 
